9301f145852a9653d8dea31787ac95f34397539f
SandyAndy <ansigida77@gmail.com>
2022-10-05 23:12:53 +0300

    Третья задача

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+string a1 = Console.ReadLine();
+int a = int.Parse(a1);
+int n = a%2;
+
+if (n != 0)
+{
+Console.WriteLine("Число нечетное.");
+}
+else
+{
+Console.WriteLine("Число четное.");
+}
