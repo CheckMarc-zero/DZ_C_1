f7d152d164b980252115ce37a62b9b02c9ad7c55
SandyAndy <ansigida77@gmail.com>
2022-10-05 22:29:07 +0300

    Первая задача

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите a:");
+string a = Console.ReadLine();
+int c = int.Parse(a);
+Console.WriteLine("Введите b:");
+string b = Console.ReadLine();
+int d = int.Parse(b);
+if (c<d)
+{
+Console.WriteLine("a < b");
+}
+else
+{
+Console.WriteLine("a > b");
+}
