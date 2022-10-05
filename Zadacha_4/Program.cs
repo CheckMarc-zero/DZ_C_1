14ff0ce75e2b37fb645403a6a39daf5f0751d137
SandyAndy <ansigida77@gmail.com>
2022-10-05 22:53:47 +0300

    Вторая задача

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите a:");
+string a1 = Console.ReadLine();
+int a = int.Parse(a1);
+Console.WriteLine("Введите b:");
+string b1 = Console.ReadLine();
+int b = int.Parse(b1);
+Console.WriteLine("Введите c:");
+string c1 = Console.ReadLine();
+int c = int.Parse(c1);
+
+int max = a;
+
+if (max<b)
+{
+max = b;
+}
+
+if (max<c)
+{
+max = c;
+}
+
+Console.Write("Наибольшее число : ");
+Console.Write(max);
