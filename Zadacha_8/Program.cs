ef6b807478b94cbf97bdd27591c5220acb042960
SandyAndy <ansigida77@gmail.com>
2022-10-05 23:28:51 +0300

    Четвёртая задача

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число N:");
+string N1 = Console.ReadLine();
+int N = int.Parse(N1);
+int x = 2;
+
+if (N<2)
+{
+  Console.WriteLine("Четных чисел от 1 до N нет.");  
+}
+
+while (x<=N)
+{
+   Console.Write(x); 
+if (x<(N-1)) 
+{  
+Console.Write(",");
+}
+x = x+2;
+}
+
+
